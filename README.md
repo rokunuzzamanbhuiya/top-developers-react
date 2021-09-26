64845e49460ef5678e0f5fd7716a3f09963a26b1
Rokunuzzaman Bhuiya <rokunuzzamanbhuiya@gmail.com>
2021-09-26 22:12:01 +0600

    all complete

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ Website link: [Top Developers](https://top-developers-react.netlify.app/).
 
 ## Our Facilities:
 
-1. Hire Top Developers For Your Projects.
-2. They are super skill for any kind of project.
-3. You can hire our expert developrs any time.
-4. For develop any site you can contact with us.
+ Markup : * Hire Top Developers For Your Projects.
+ Markup : * They are super skill for any kind of project.
+ Markup : * You can hire our expert developrs any time.
+ Markup : * For develop any site you can contact with us.
 
 ### `All Right Reserved By Rokunuzzaman Bhuiya.`
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,20 +6,20 @@ const Header = () => {
       <>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-4">
         <div className="container">
-          <a className="navbar-brand" href="{#}"><h2>Top Developers</h2></a>
+          <a className="navbar-brand" href="/"><h2>Top Developers</h2></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="{#}">Home</a>
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="{#}">Developers</a>
+                <a className="nav-link active" aria-current="page" href="/">Developers</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="{#}">Details</a>
+                <a className="nav-link active" aria-current="page" href="/">Details</a>
               </li>
             </ul>
             <form className="d-flex">
